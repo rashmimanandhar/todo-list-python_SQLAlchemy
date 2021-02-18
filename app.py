5108c824dd3c935d28d27563e4189093c377f66f
Rashmi <rashmi.manandhar7@gmail.com>
2021-02-17 19:36:50 -0500

    feat: connect to database and read and show in template

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rashmi:rashmi@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<TODO {self.id}, {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    render_template('index.html', data=[{
-        'description': 'Todo 1'
-    },{
-        'description': 'Todo 2'
-    },{
-        'description': 'Todo 3'
-    }])
+    return render_template('index.html', data=Todo.query.all())
