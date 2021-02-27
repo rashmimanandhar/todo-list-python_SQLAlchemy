72a00202bf71cebc734bfced664434d80e1cd3f5
Rashmi <rashmi.manandhar7@gmail.com>
2021-02-27 15:46:46 -0500

    feat: use migration from flask_migrate to create the todos db

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rashmi:rashmi@localhost:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
@@ -15,7 +18,7 @@ class Todo(db.Model):
     def __repr__(self):
         return f'<TODO {self.id}, {self.description}>'
 
-db.create_all()
+# db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
